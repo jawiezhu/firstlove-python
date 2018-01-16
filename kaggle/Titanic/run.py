e0304eeb9872040eca1cbdd6262b3f3460e5de1a
jawiezhu <406934590@qq.com>
2018-01-16 15:13:23 +0800

    0116

--- kaggle/Titanic/run.py
+++ kaggle/Titanic/run.py
@@ -3,15 +3,17 @@
 from get_data_report import *
 from preprocess_for_data import *
 
-RAW_TRAIN_FILE = r'train.csv'
-RAW_TEST_FILE = r'test.csv'
-RESULT_TRAIN_PATH = r'res_train.csv'
-RESULT_TEST_PATH = r'res_test.csv'
+RAW_TRAIN_FILE = r'./data/train.csv'
+RAW_TEST_FILE = r'./data/test.csv'
+RESULT_TRAIN_PATH = r'./data/res_train.csv'
+RESULT_TEST_PATH = r'./data/res_test.csv'
+
 
 def main():
     get_data_report(RAW_TRAIN_FILE)
     preprocess_for_train_data(RAW_TRAIN_FILE, RESULT_TRAIN_PATH)
     preprocess_for_test_data(RAW_TEST_FILE, RESULT_TEST_PATH)
+
 
 if __name__ == '__main__':
     main()
